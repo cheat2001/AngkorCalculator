0a32f7c72d13b93fedd2fe750e85c756afae12b8
cheat2001 <83935278+cheat2001@users.noreply.github.com>
2021-07-07 20:35:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const get=document.querySelector('.equal');
-const scr=document.querySelector('.screen');
-
-get.addEventListener('click',getvalue);
-
-function getvalue(){
-  let got=eval(scr.value);
-  scr.value=got;
-}
